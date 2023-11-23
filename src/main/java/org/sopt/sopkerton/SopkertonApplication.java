8f3664f0cc6a4881622442d9669109aa7bfb0404
transfer_kk <kseysh123@naver.com>
2023-11-24 01:29:25 +0900

    feat: 초기 프로젝트 설정

---
+++ src/main/java/org/sopt/sopkerton/SopkertonApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.sopkerton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopkertonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SopkertonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sopt/sopkerton/SopkertonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.sopkerton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SopkertonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
