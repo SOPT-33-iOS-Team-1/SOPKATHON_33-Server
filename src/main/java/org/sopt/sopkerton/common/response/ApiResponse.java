a5da34c88fb5c85e3b9e4de54f53a847d1b1fa0d
yummygyudon <bang2brew@gmail.com>
2023-11-24 23:48:54 +0900

    FEAT : Response DTO 구현
    - Thanks to 동섭 & 승환

---
+++ src/main/java/org/sopt/sopkerton/common/response/ApiResponse.java
@@ -0,0 +1,38 @@
+package org.sopt.sopkerton.common.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.sopt.sopkerton.common.exception.base.ErrorBase;
+import org.sopt.sopkerton.common.exception.base.SuccessBase;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse<T> {
+    private final int code;
+    private final String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
+
+    public static ApiResponse<?> success(SuccessBase success) {
+        return new ApiResponse<>(success.getHttpStatusCode(), success.getSuccessMessage());
+    }
+
+    public static <T> ApiResponse<T> success(SuccessBase success, T data) {
+        return new ApiResponse<T>(success.getHttpStatusCode(), success.getSuccessMessage(), data);
+    }
+
+    public static <T> ApiResponse <T> error(ErrorBase error) {
+        return new ApiResponse<>(error.getHttpStatusCode(), error.getErrorMessage());
+    }
+
+    public static <T> ApiResponse<T> error(ErrorBase error, T data) {
+        return new ApiResponse<>(error.getHttpStatusCode(), error.getErrorMessage(), data);
+    }
+
+    public static ApiResponse <Exception> error(ErrorBase error, Exception exception) {
+        return new ApiResponse<>(error.getHttpStatusCode(), error.getErrorMessage(), exception);
+    }
+}
