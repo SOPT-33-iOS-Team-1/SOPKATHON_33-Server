56d321432289a77a2e28218a8253973b2569b13b
yummygyudon <bang2brew@gmail.com>
2023-11-25 03:10:38 +0900

    FIX : 부모 클래스로만 쓰여야 하기 때문에 추상클래스로 변환

--- src/main/java/org/sopt/sopkerton/common/exception/base/ExceptionBase.java
+++ src/main/java/org/sopt/sopkerton/common/exception/base/ExceptionBase.java
@@ -1,21 +1,20 @@
 package org.sopt.sopkerton.common.exception.base;
 
-import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 /**
- * == 하위 도메인 예외는 본 Exception 클래스를 상속 받습니다.  ==
+ * 하위 도메인 예외는 본 Exception 클래스를 상속 받습니다.
+ * - 객체 생
  */
-@Getter
-public class ExceptionBase extends RuntimeException{
+public abstract class ExceptionBase extends RuntimeException{
     private static final String ERROR_MESSAGE_HEADER = "ERROR : ";
 
     private final ErrorBase errorBase;
-    public ExceptionBase(ErrorBase errorBase) {
+    protected ExceptionBase(ErrorBase errorBase) {
         super(ERROR_MESSAGE_HEADER + errorBase.getErrorMessage());
         this.errorBase = errorBase;
     }
-    public HttpStatus getStatus() {
-        return errorBase.getHttpStatus();
+
+    public ErrorBase getError() {
+        return errorBase;
     }
 }
