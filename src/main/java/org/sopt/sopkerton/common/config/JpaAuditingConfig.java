34f5a902011f1a8e93e4425c470204847c15c734
transfer_kk <kseysh123@naver.com>
2023-11-26 01:05:50 +0900

    feat: BaseEntity 세팅

---
+++ src/main/java/org/sopt/sopkerton/common/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package org.sopt.sopkerton.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
